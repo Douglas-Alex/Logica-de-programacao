3d39be7e69a47d0298e1ff8a19aa2eac8a0a31fa
Douglas <douglasalex.dab@gmail.com>
2022-05-12 17:52:20 -0300

    Exercicios de fixação

---
+++ ExercicioDeFixacao/ExercicioDeFixacao/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            int Idade;
+            double Altura;
+            int Quarto;
+            double Preco;
+
+            Console.WriteLine("Digite seu nome completo:");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            Quarto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual o valor avaliado de sua casa?");
+            Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite sua idade e altura:");
+            string[] vet = Console.ReadLine().Split(' ');
+            Idade = int.Parse(vet[0]);
+            Altura = double.Parse(vet[1]);
+
+            Console.WriteLine(Nome+ " você tem " +Idade+ " anos e " +Altura+ " de altura, sua casa possui "
+                              +Quarto+ " quarto(s) e sua casa foi avaliada em R$ " +Preco.ToString("F2")+ ".");
+        }
+    }
+}
---
+++ ExercicioDeFixacao2/ExercicioDeFixacao2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura;
+            double comprimento;
+            double precoMetroQuadrado;
+            double area;
+            double preco;
+
+            Console.WriteLine("Informe a largura do seu terreno:");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe a comprimento do seu terreno:");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Informe o preço do metro quadrado:");
+            precoMetroQuadrado = double.Parse(Console.ReadLine());
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("Area = " + area.ToString("F2", CultureInfo.InvariantCulture) + "m");
+            Console.WriteLine("Valor avaliado do terreno = R$ " + preco.ToString("F2"));
+
+
+
+
+
+
+        }
+    }
+}
