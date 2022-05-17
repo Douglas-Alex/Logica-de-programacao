78a0657808e714cdae67701ed7e7ea5eb681d78a
Douglas <douglasalex.dab@gmail.com>
2022-05-17 19:57:37 -0300

    Exercicio 3

---
+++ ExercicioDeFixacao3/ExercicioDeFixacao3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b;
+            double a;
+            double area;
+            double perimetro;
+            double diagonal;
+
+            Console.WriteLine("Qual a medida da base do seu retângulo?");
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual altura do seu retângulo?");
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = b * a;
+            perimetro = 2 * (b + a);
+            diagonal = Math.Sqrt(Math.Pow(b, 2.0) + Math.Pow(a, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
