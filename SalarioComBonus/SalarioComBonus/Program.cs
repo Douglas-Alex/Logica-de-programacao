a3cefbb63bd7591bfa9fbea4b1418ee3244661b2
Douglas <douglasalex.dab@gmail.com>
2022-05-18 19:42:27 -0300

    Salário com bonus

---
+++ SalarioComBonus/SalarioComBonus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SalarioComBonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioFixo;
+            double totalVendas;
+            double salarioReceber;
+
+            Console.WriteLine("Nome do vendedor(a):");
+            nome = Console.ReadLine();
+            Console.WriteLine("Salario fixo vendedor(a):");
+            salarioFixo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Total de vendas efetuada no mês pelo vendedor(a):");
+            totalVendas = double.Parse(Console.ReadLine());
+
+            salarioReceber = ((double)15/100)* totalVendas + salarioFixo;
+
+            Console.WriteLine(nome+" vai receber R$ " +salarioReceber.ToString("F2"));
+        }
+    }
+}
